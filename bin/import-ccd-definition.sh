42ce2ee5133e7d6d58e91792212ad3dc5c528719
vasudevganesanhmcts <100689363+vasudevganesanhmcts@users.noreply.github.com>
2023-06-28 09:25:57 +0100

    modifying the script

--- bin/import-ccd-definition.sh
+++ bin/import-ccd-definition.sh
@@ -15,8 +15,17 @@ cd civil-ccd-definition
 
 echo "Switch to ${branchName} branch on civil-ccd-definition"
 git checkout ${branchName}
+cd ..
 
+#Copy ccd definition files  to civil-ccd-def which contians bpmn files
+cp -r ./civil-general-apps-ccd-definition/ga-ccd-definition .
+cp -r ./civil-general-apps-ccd-definition/e2e .
+cp -r ./civil-general-apps-ccd-definition/package.json .
+cp -r ./civil-general-apps-ccd-definition/yarn.lock .
+cp -r ./civil-general-apps-ccd-definition/codecept.conf.js .
+echo *
 
+cd civil-ccd-definition
 definition_input_dir=$(realpath './ccd-definition')
 definition_output_file="$(realpath ".")/ccd-definition/build/ccd-development-config/ccd-civil-dev.xlsx"
 
