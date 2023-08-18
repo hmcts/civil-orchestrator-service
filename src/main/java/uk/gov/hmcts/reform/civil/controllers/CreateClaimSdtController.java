df419e91f02d94ca0b38ddd794bbd26313e1ee17
Deepthi Doppalapudi <107422736+deepthidoppalapudihmcts@users.noreply.github.com>
2023-08-18 12:55:31 +0100

    Revert "CIV-9252 removed search"
    This reverts commit a2d8834e38157cc2c5c36a1aa6f79223118ac4c6.

--- src/main/java/uk/gov/hmcts/reform/civil/controllers/CreateClaimSdtController.java
+++ src/main/java/uk/gov/hmcts/reform/civil/controllers/CreateClaimSdtController.java
@@ -43,7 +43,7 @@ public class CreateClaimSdtController {
     }
 
     private void validateSdtRequestId(String authorization, String sdtRequestId) {
-        boolean sdtRequestIdFromCcd = true;
+        boolean sdtRequestIdFromCcd = createClaimFromSdtService.validateSdtRequest(authorization,sdtRequestId);
         if (!sdtRequestIdFromCcd) {
             throw new ApplicationException(ErrorDetails.INVALID_DATA, HttpStatus.BAD_REQUEST, "Request already processed");
         }
