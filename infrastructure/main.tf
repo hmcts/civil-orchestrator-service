2e3ab9853a7abefba1f97a489144723bd2e2e7ea
58046 <62422075+hmcts-jenkins-a-to-c[bot]@users.noreply.github.com>
2024-06-28 06:53:38 +0000

    Updating Terraform Formatting

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -6,7 +6,7 @@ provider "azurerm" {
 resource "azurerm_resource_group" "rg" {
   name     = "${var.product}-${var.component}-${var.env}"
   location = var.location
-  tags = var.common_tags
+  tags     = var.common_tags
 }
 
 data "azurerm_key_vault" "civil" {
